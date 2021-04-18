6c0fca76b5eb1c4384488ea83773dcc02b3c982d
Ziya Mamedov <ziya_90@mail.ru>
2021-04-18 18:58:37 +0300

    added cart

--- src/components/ShopCartDisplay.js
+++ src/components/ShopCartDisplay.js
@@ -1,9 +1,24 @@
+import { useSelector } from "react-redux";
+
 function ShopCartDisplay() {
+  const products = useSelector((store) => store.products);
+  let prodQuanTotal = 0;
+
+  products.forEach((prod) => {
+    prodQuanTotal += prod.orderedQuan;
+  });
+
   return (
-    <nav className="navbar navbar-light bg-light">
-      <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1">Cart</span>
+    <nav
+      className="navbar navbar-light bg-light"
+      style={{ marginBottom: "20px" }}
+    >
+      <div className="container-fluid justify-content-start">
+        <span className="navbar-brand mb-0 h1">Cart:</span>
+        <span className="navbar-brand mb-0 h1">{prodQuanTotal}</span>
       </div>
     </nav>
   );
 }
+
+export default ShopCartDisplay;
